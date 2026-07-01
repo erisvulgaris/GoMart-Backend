1b6bdd556f9fcac320f2355ea69753d8eacdf77d
erisvulgaris <erisvulgaris@users.noreply.github.com>
2026-07-01 18:22:03 +0530

    temp: add tables checker

---
+++ public/db_tables.php
@@ -0,0 +1,16 @@
+<?php
+header('Content-Type: application/json');
+$db = new mysqli("db", "gomart", "gomart_secure_pass", "gomart");
+if ($db->connect_error) {
+    echo json_encode(["error" => "Connection failed: " . $db->connect_error]);
+    exit;
+}
+
+$res = $db->query("SHOW TABLES");
+$tables = [];
+while ($row = $res->fetch_array()) {
+    $tables[] = $row[0];
+}
+
+echo json_encode(["tables" => $tables], JSON_PRETTY_PRINT);
+$db->close();
