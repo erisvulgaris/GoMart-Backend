343bc4705ed4918c56ba2d088282aa2c86903142
erisvulgaris <erisvulgaris@users.noreply.github.com>
2026-07-01 22:54:34 +0530

    cleanup log reader

--- public/remote_logs.php
+++
@@ -1,9 +0,0 @@
-<?php
-header('Content-Type: text/plain');
-$logFile = '/var/www/html/writable/logs/log-2026-07-01.log';
-if (file_exists($logFile)) {
-    $content = file_get_contents($logFile);
-    echo substr($content, -15000);
-} else {
-    echo "Log file does not exist: " . $logFile;
-}
