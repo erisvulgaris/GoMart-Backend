cb74a55ecdd1f8dea420f366309ab8a6199b38c6
erisvulgaris <erisvulgaris@users.noreply.github.com>
2026-07-02 11:00:41 +0530

    temp: fix get_latest_otp.php

--- public/get_latest_otp.php
+++ public/get_latest_otp.php
@@ -5,7 +5,7 @@ if ($db->connect_error) {
     echo json_encode(["error" => "Connection failed: " . $db->connect_error]);
     exit;
 }
-$res = $db->query("SELECT otp FROM otp_log WHERE mobile = '7007691934' ORDER BY id DESC LIMIT 1;");
+$res = $db->query("SELECT otp FROM otp_verification WHERE mobile = '7007691934' ORDER BY id DESC LIMIT 1;");
 $row = $res->fetch_assoc();
 echo json_encode(["otp" => $row ? $row['otp'] : null]);
 $db->close();
