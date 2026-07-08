b8feb1ef0ddbd06740958ccc8bb828693f1f3b61
erisvulgaris <erisvulgaris@users.noreply.github.com>
2026-07-08 09:53:18 +0530

    temp: add check timeslots script

---
+++ public/run_check_timeslots.php
@@ -0,0 +1,15 @@
+<?php
+header('Content-Type: application/json');
+$db = new mysqli("db", "gomart", "gomart_secure_pass", "gomart");
+if ($db->connect_error) {
+    echo json_encode(["error" => "Connection failed: " . $db->connect_error]);
+    exit;
+}
+
+$query = $db->query("SELECT * FROM timeslot");
+$results = [];
+while ($row = $query->fetch_assoc()) {
+    $results[] = $row;
+}
+echo json_encode($results, JSON_PRETTY_PRINT);
+$db->close();
