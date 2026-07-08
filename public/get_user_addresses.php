e7420868eefabb78d918342a5889c2a297e4f5ec
erisvulgaris <erisvulgaris@users.noreply.github.com>
2026-07-08 12:07:03 +0530

    temp: add get user addresses script

---
+++ public/get_user_addresses.php
@@ -0,0 +1,31 @@
+<?php
+header('Content-Type: application/json');
+$db = new mysqli("db", "gomart", "gomart_secure_pass", "gomart");
+if ($db->connect_error) {
+    echo json_encode(["error" => "Connection failed: " . $db->connect_error]);
+    exit;
+}
+
+// Find user Sakshi Shukla
+$userQuery = $db->query("SELECT * FROM users WHERE mobile = '9651112348'");
+$user = $userQuery->fetch_assoc();
+
+if (!$user) {
+    echo json_encode(["error" => "User not found"]);
+    $db->close();
+    exit;
+}
+
+// Find addresses
+$addrQuery = $db->query("SELECT * FROM addresses WHERE user_id = " . $user['id'] . " AND is_delete = 0");
+$addresses = [];
+while ($row = $addrQuery->fetch_assoc()) {
+    $addresses[] = $row;
+}
+
+echo json_encode([
+    "user" => $user,
+    "addresses" => $addresses
+], JSON_PRETTY_PRINT);
+
+$db->close();
